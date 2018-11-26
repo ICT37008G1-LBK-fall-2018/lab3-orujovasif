b0a37751e2b23baad2541740881b25c1a96c21d2
Sergi <sergi199508@gmail.com>
2018-11-26 10:09:57 +0400

    l301

---
+++ L301/script.js
@@ -0,0 +1,11 @@
+let p = prompt("Enter String");
+if (p.match("reklama")) {
+    alert("akrdaluli sityva: reklama !!");
+}
+if (p.match("marketingi")) {
+    alert("akrdzaluli sityva: marketingi !!");
+}
+if (p.match("virusi")) {
+    alert("akrdzaluli sityva: virusi");
+}
+alert("You Entered: " + p);
